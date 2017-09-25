2266789b40a30611219f6078c1cd6a663f120515
Aqsa099 <aqsachoudhary402@gmail.com>
2017-09-25 11:20:05 +0500

    view attendance

---
+++ attendance.php
@@ -0,0 +1,30 @@
+<?php include_once 'header.php'; ?>
+<?php include_once 'sidebar.php'; ?>
+<!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1 class="well well-sm" style="border-top: 3px solid teal; text-align:center">
+        Attendance
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+	<div class="row">
+		<div class="col-md-3">
+		</div>
+		
+		<div class="col-md-7">
+			<div class="box-body">
+			</div>
+		</div>
+		
+		<div class="col-md-3">
+		</div>
+	</div>	
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php include_once 'footer.php'; ?>
